517a5453d6319ef466ba0f94c00e826185cdcc7c
alexd106 <alexd106@gmail.com>
2019-12-09 10:52:51 +0000

    rebuild

--- code_fragments.R
+++ code_fragments.R
@@ -2,3 +2,9 @@
 # ```{r fig.align='center', echo=FALSE, out.width="50%", out.height="50%"}
 # knitr::include_graphics('images/rbook_cover3.png', dpi = NA)
 # ```
+
+
+# options(
+#	htmltools.dir.version = FALSE, formatR.indent = 2,
+#	width = 55, digits = 4, warnPartialMatchAttr = FALSE, warnPartialMatchDollar = FALSE)
+
