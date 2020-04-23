8b147b975d3019ffd6129dffd4a9650cc81c5734
alexd106 <alexd106@gmail.com>
2020-04-23 13:37:44 +0100

    add who are we ouput agnostic

--- code_fragments.R
+++ code_fragments.R
@@ -47,14 +47,89 @@
 # 						><p style="font-weight:400; font-size:16px">Benthic Biodiversity experiment.</p>
 # 							><p></p>
 							
-							
+<div style= "float:left;position: relative; top: 5px;">
+	```{r, out.width = "200px", out.extra='style="padding:30px;"', echo = FALSE}
+knitr::include_graphics("images/alex.png")
+```
+</div>
+	
+	\ 
+
+**Alex Douglas** is a Reader in Biostatistics at the University of Aberdeen and has been teaching (and learning) R for the last 15 years. 
+
+- Web: https://www.abdn.ac.uk/people/a.douglas/
+	- Twitter: https://twitter.com/Scedacity
+- GitHub: https://github.com/alexd106
+
+\ 
 
 \  
 
-image
+<div style= "float:left;position: relative; top: 5px;">
+	```{r, out.width = "200px", out.extra='style="padding:30px;"', echo = FALSE}
+knitr::include_graphics("images/deon.png")
+```
+</div>
+	
+	\  
+
+**Deon Roos** is a PhD student in applied ecology at the University of Aberdeen researching spatial variation of the temporal kind in population dynamics. He has been using and teaching R for 5 years. 
+
+- Web: https://www.abdn.ac.uk/people/deon.roos
+- Twitter: https://twitter.com/DeonRoos88
+- GitHub: https://github.com/Deon88
 
 \  
 
+\  
 
-)				
+<div style= "float:left;position: relative; top: 5px;">
+	```{r, out.width = "200px", out.extra='style="padding:30px;"', echo = FALSE}
+knitr::include_graphics("images/ana.png")
+```
+</div>
+	
+	\  
+
+**Ana Couto** is a PhD student in Ecology at the University of Aberdeen looking at the ecology and physics of tidal stream habitats. She has been using and teaching R for 7 years. 
+
+- Web: https://www.abdn.ac.uk/people/r01ac17
+- Twitter: https://twitter.com/AnaSofiaCouto
+- GitHub:  https://github.com/Anofia
+
+\  
+
+\  
+
+<div style= "float:left;position: relative; top: 5px;">
+	```{r, out.width = "200px", out.extra='style="padding:30px;"', echo = FALSE}
+knitr::include_graphics("images/francesca2.png")
+```
+</div>
+	
+	\  
+
+**Francesca Mancini** is an ecological modeller, working with large citizen science datasets and statistical models to derive trends in biodiversity change and investigate its drivers.
+
+- Web: https://FrancescaMancini.github.io
+- Twitter: https://twitter.com/frances_mancini
+- GitHub:  https://github.com/FrancescaMancini
+
+\  
+
+\  
+
+<div style= "float:left;position: relative; top: 5px;">
+	```{r, out.width = "200px", out.extra='style="padding:30px;"', echo = FALSE}
+knitr::include_graphics("images/lusseau.png")
+```
+</div>
+	
+	\  
+
+**David Lusseau** is a Professor of Marine Sustainability at the Danish Technical University. He's been using R for 15 years and teaching it for 12 years.
+
+- Twitter: https://twitter.com/lusseau
+- GitHub:  https://github.com/dlusseau
+			
 	
